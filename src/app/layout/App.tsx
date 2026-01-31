79d5691188a48f0f89ba7340e1ede7e9a69be7fc
adham-hashem <adhamhashem2024@gmail.com>
2026-01-31 17:50:36 +0200

    feat: install vercel libraries

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { CompilationProvider } from "../../features/context/CompilationContext";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App: React.FC = () => {
   return (
     <CompilationProvider>
       <Outlet />
+      <Analytics />
+      <SpeedInsights />
     </CompilationProvider>
   );
 };
